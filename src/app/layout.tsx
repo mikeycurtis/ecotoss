61ac1e57bfb5797058a9f1fa72625133f6c42844
Mikey Curtis <mjcurtis@umail.ucsb.edu>
2024-04-08 17:52:59 -0700

    working on main hero

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./ui/globals.css";
 import NavBar from "./ui/navbar";
 import { Providers } from "./providers";
-import localFont from "@next/font/local";
-
-const inter = Inter({ subsets: ["latin"] });
-
-// import moreganicFont from "@fonts/moreganic-font";
-
-// const moreganic = localFont({
-//   src: [
-//     {
-//       path: "../../public/fonts/Moreganic.woff",
-//       weight: "400",
-//     },
-//   ],
-//   variable: "--font-moreganic",
-// });
 
 export const metadata: Metadata = {
   title: "EcoToss Compostable Paper Bags",
--- src/app/ui/landing/main-hero.tsx
+++ src/app/ui/landing/main-hero.tsx
@@ -1,17 +1,35 @@
 import Image from "next/image";
 export default function MainHero() {
   return (
-    <div
-      className="bg-red-300 h-[500px] bg-cover bg-center"
-      style={{ backgroundImage: `url("/images/mainheroimage.jpg")` }}
-    >
-      <p className="text-white">
-        Organic Waste
-        <br />
-        Bags For a <span className="text-lightgreen">Greener</span>
-        <br />
+    <div className="border border-2 h-auto bg-cover bg-center bg-stone lg:flex">
+      <LeftArea />
+      <RightArea />
+    </div>
+  );
+}
+
+function LeftArea() {
+  return (
+    <div className="bg-blue-200 w-full h-auto px-10 py-16 md:h-full flex-row items-center justify-center">
+      <p className="text-white text-4xl text-left">
+        Organic Waste <br />
+        Bags For a <span className="text-lightgreen">Greener</span> <br />
         San Diego
       </p>
+      <p className="mt-4">
+        Our water-repellent paper bags are designed for San Diego's composting
+        system, ensuring strength and durability. They are the perfect solution
+        and size for your kitchen pails.
+      </p>
+    </div>
+  );
+}
+
+function RightArea() {
+  return (
+    <div className="bg-red-200 w-full h-auto flex-row items-center justify-center px-10 py-16">
+      <div className="bg-white w-100 h-40"></div>
+      <button className="bg-blue-200 w-24 h-10 mt-5">Order Now</button>
     </div>
   );
 }
